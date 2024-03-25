1d8402dee518539d425b4319a6324e4fd776c875
DJ-ust <121188328+DJ-ust@users.noreply.github.com>
2024-03-25 06:01:32 +0100

    test

---
+++ accueil.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Suzanne Valadon | Une vision de l’imperfection</title>
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="style.css">
+
+
+</head>
+
+<body>
+
+
+
+    <section id="hero">
+        <header class="header">
+            <a href="#" class="logo"><img src="img/éléments/Logo-SV-blanc 1.svg" id="oeil" alt=""></a>
+            <i class='bx bx-menu' id="menu-icon"></i>
+            <nav class="navbar">
+                <a href="#home" class="active">Accueil</a>
+                <a href="#about">Suzanne.V</a>
+                <a href="#services">Exposition</a>
+                <a href="reservation.php">Réserver</a>
+
+            </nav>
+        </header>
+        <!-- <nav>
+            <a href="" class="lien">Accueil</a>
+            <a href="" class="lien">Exposition</a>
+            <a href="" class="lien">Suzanne.V</a>
+            <a href="" class="lien">Réserver</a>
+        </nav> -->
+
+        <div id="bloc_titre">
+            <h1>Suzanne Valadon</h1>
+            <h2 class="sous_titre">Une vision de l’imperfection</h2>
+            <p id="description">Plongez dans le regard de Suzanne Valadon, à travers une image de la femme subjective et contemporaine, dans une époque où la liberté féminine furent limitées par les moeurs. </p>
+
+            <a href="accueil.php">
+                <div id="button">Réserver</div>
+            </a>
+        </div>
+
+    </section>
+
+    <section id="second">
+        <div id="contenu_second">
+            <div id="titre_coordonnees">Découvrir l’art en toute immersion</div>
+            <div id="coordonnees">
+                <div class="co">
+                    <div class="icon clock"><img src="img/éléments/icon _clock_.svg" alt=""></div>
+                    <p>10h - 18h</p>
+                </div>
+                <div class="co">
+                    <div class="icon map"><img src="img/éléments/icon _map marker_.svg" alt=""></div>
+                    <p>
+                        2 rue Albert Einstein <br>
+                        CHAMPS-SUR-MARNE 77420
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="banderole">
+        <div id="banderole_partie_1"><img src="img/tableaux/" alt=""></div>
+        <div id="banderole_partie_2"></div>
+    </section>
+
+    <section id="third">
+        <h2>Plongez dans les regards</h2>
+        <div id="contenu_third">
+
+            <p class="paragraphe">Pourquoi représenter les femmes d'un air parfait ? Pourquoi les rougeurs, les cernes ou encore les formes, ce qui fait que chaque femme est unique devrait être camouflés ?
+                Suzanne Valadon peignait ces "imperfections" pour montrer que le corps de la Femme n'est pas un objet (et qu'il est tel qu'il est).
+                <br><br>
+                " Il faut avoir le courage de regarder le modèle en face si l’on veut atteindre l’âme. Ne m’amenez jamais une femme qui cherche l’aimable ou le joli, je la décevrai tout de suite."
+            </p>
+            <figure>
+                <img src="img/tableaux/automne.jpg" alt="Teaser de l’exposition de Suzanne Valadon, 
+“Une vision de l’imperfection”." />
+
+                <figcaption>
+                Teaser de l’exposition de Suzanne Valadon, 
+“Une vision de l’imperfection”.
+                </figcaption>
+            </figure>
+
+        </div>
+    </section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <footer>
+
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.12"></script>
+    <script src="script.js"></script>
+</body>
+
+</html>
---
+++ reservation.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Réservation | une vision de l'imperfection</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<header class="header">
+        <a href="#" class="logo"><img src="img/éléments/Logo-SV-blanc 1.svg" id="oeil" alt=""></a>
+        <i class='bx bx-menu' id="menu-icon"></i>
+        <nav class="navbar">
+            <a href="#home" class="active">Accueil</a>
+            <a href="#about">Suzanne.V</a>
+            <a href="#services">Exposition</a>
+            <a href="index.php?action=reservation.php">Réserver</a>
+
+        </nav>
+    </header>
+</body>
+</html>
